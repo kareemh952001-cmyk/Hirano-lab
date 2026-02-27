400aaec62e433f6bd45b68bf4943f3c64e89563a
kareemh952001-cmyk <kareemh952001@gmail.com>
2026-02-27 16:02:32 +0900

    Update script.js

--- script.js
+++ script.js
@@ -2,10 +2,13 @@ const elements = document.querySelectorAll('.fade-in');
 
 const observer = new IntersectionObserver(entries => {
   entries.forEach(entry => {
-    if(entry.isIntersecting){
+    if (entry.isIntersecting) {
       entry.target.classList.add('visible');
     }
   });
+}, {
+  threshold: 0.4,                // 40% of element must be visible
+  rootMargin: "0px 0px -120px 0px" // pushes trigger lower
 });
 
 elements.forEach(el => observer.observe(el));
